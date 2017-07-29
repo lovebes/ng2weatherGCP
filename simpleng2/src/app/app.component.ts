7f51a90d970f4a89f83b7754e19d20c30dbb5124
Seungjin Kim <skim@proove.com>
2017-07-28 19:06:32 -0700

    remove wrong attempt

--- simpleng2/src/app/app.component.ts
+++ simpleng2/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit{
   weatherData:any[] = [];
 
   constructor(private weatherService: WeatherService){}
-  
+
   ngOnInit(){
 
     let today = new Date();
